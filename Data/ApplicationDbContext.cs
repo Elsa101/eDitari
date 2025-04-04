0f6b591be465524d4d5028aae2a4900d17c971f8
Elsa101 <elsa.isufi14@gmail.com>
2025-04-05 01:16:40 +0200

    lidhja me databaze

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace eDitari.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Grade> Grades { get; set; }
+        public DbSet<Attendance> Attendances { get; set; }
+    }
+}
