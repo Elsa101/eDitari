35ab36f625ae0a0c08325173561f71e020f0ca04
KlevisHaraqija <klevisharaqija@hotmail.com>
2025-05-11 22:37:29 +0200

    controllers

--- Editari/CRUD/GradesController.cs
+++ Editari/CRUD/GradesController.cs
@@ -59,3 +59,63 @@ namespace Editari.Controllers
         }
     }
 }
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Editari.Data;
+using Editari.Models;
+
+namespace Editari.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CommentsController : ControllerBase
+    {
+        private readonly EditariDbContext _context;
+
+        public CommentsController(EditariDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comment>>> GetAll()
+        {
+            return await _context.Comments.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comment>> Get(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null) return NotFound();
+            return comment;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Comment>> Post(Comment comment)
+        {
+            _context.Comments.Add(comment);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(Get), new { id = comment.CommentId }, comment);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Comment comment)
+        {
+            if (id != comment.CommentId) return BadRequest();
+            _context.Entry(comment).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var comment = await _context.Comments.FindAsync(id);
+            if (comment == null) return NotFound();
+            _context.Comments.Remove(comment);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
