ea09227726e9c821988b8e89d15edc310645b93f
Elsa101 <elsa.isufi14@gmail.com>
2026-01-30 23:40:38 +0100

    backend testing and edits

---
+++ Editari/Migrations/20260130204436_Baseline.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eDitari.Migrations
+{
+    /// <inheritdoc />
+    public partial class Baseline : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Baseline migration – database already exists
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // No rollback for baseline
+        }
+    }
+}
